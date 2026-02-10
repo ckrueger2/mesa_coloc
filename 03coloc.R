e66eb90cdb1c2fa30460ca410422ce61785ffe68
Claudia Krueger <ckrueger2@luc.edu>
2026-02-09 23:08:04 -0600

    Update 03coloc.R

--- 03coloc.R
+++ 03coloc.R
@@ -125,8 +125,8 @@ if (length(common_variants) > 0) {
   
   #assign outputs
   cc <- phe_counts[[phecode]][[aou_pop]]
-  aou_cases    <- as.integer(cc["case"])
   aou_pop_size <- as.integer(cc["case"] + cc["control"])
+  aou_cases    <- as.integer(cc["case"]/aou_pop_size)
   
   #prepare datasets
   dataset1 <- list(
