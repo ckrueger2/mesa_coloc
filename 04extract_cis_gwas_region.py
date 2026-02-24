12cead8679f294e8966983a4d773e01b55b6a431
Claudia Krueger <ckrueger2@luc.edu>
2026-02-24 11:20:05 -0600

    Update 04extract_cis_gwas_region.py

--- 04extract_cis_gwas_region.py
+++ 04extract_cis_gwas_region.py
@@ -98,7 +98,7 @@ print(f"Table dimensions: {rows} rows x {cols} columns")
 ordered_ht_chr = ordered_ht.filter(ordered_ht.CHR == chrom)
 extracted_region = ordered_ht_chr.filter(left < ordered_ht_chr.POS < right)
 num_snps = extracted_region.count()
-print(f"Number of SNPs: {num_snps})
+print(f"Number of SNPs: {num_snps}")
 
 #save filtered file
 filtered_path = f'{bucket}/data/{pop}_cis_region_{phenotype_id}.tsv'
